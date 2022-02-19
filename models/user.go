8d0704edcd34bf99b6a48c3f90d580ec5b8d0ef2
dicky <dicky@dfx-id.com>
2022-02-19 21:34:48 +0700

    handle json output

--- models/user.go
+++ models/user.go
@@ -1,9 +1,9 @@
 package models
 
 type User struct {
-	Id        string
-	FirstName string
-	LastName  string
-	Email     string `gorm:"unique"`
-	Password  []byte
+	Id        string `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email" gorm:"unique"`
+	Password  []byte `json:"-"`
 }
